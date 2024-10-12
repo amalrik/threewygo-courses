23bff906d420ed11ceae2c667f13c7d29b9af940
amalrik <amalrik.maia@gmail.com>
2024-10-12 14:44:25 -0300

    shoulda-matchers validations added

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -4,4 +4,10 @@ describe Course do
   it "has a valid factory" do
     expect(FactoryBot.build(:course)).to be_valid
   end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:start_at) }
+    it { should validate_presence_of(:end_at) }
+  end
 end
