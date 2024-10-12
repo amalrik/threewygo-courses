4f9c222b36aa50bb890d0e3bfcac97b3bde0873f
amalrik <amalrik.maia@gmail.com>
2024-10-12 15:06:43 -0300

    current courses scope

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ApplicationRecord
   validates :title, presence: true
   validates :start_at, presence: true
   validates :end_at, presence: true
+
+  scope :current, -> { where("end_at >= ?", Date.today) }
 end
--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe Course do
+  let(:course) { FactoryBot.create(:course, end_at: end_at) }
+
   it "has a valid factory" do
     expect(FactoryBot.build(:course)).to be_valid
   end
@@ -9,5 +11,22 @@ describe Course do
     it { should validate_presence_of(:title) }
     it { should validate_presence_of(:start_at) }
     it { should validate_presence_of(:end_at) }
+  end
+
+  describe '.current' do
+    context 'when end_date is older than today' do
+      let(:end_at) { 1.week.ago }
+      it 'does not display expired courses' do
+        expect(Course.current).to be_empty
+      end
+    end
+
+    context 'when end_date is in the future' do
+      let(:end_at) { 1.week.from_now }
+
+      it 'does display current courses' do
+        expect(Course.current).to contain_exactly(course)
+      end
+    end
   end
 end
